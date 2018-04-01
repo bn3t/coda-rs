8cbd6dc2f3be696b8c717e4b33a63a9283433764
Bernard Niset <bernard.niset@gmail.com>
2018-04-02 01:27:24 +0200

    Parse movements (work in progess)

--- src/coda.rs
+++ src/coda.rs
@@ -6,7 +6,8 @@ use std::io::{BufRead, BufReader};
 use chrono::NaiveDate;
 
 use errors::*;
-use utils::{parse_date, parse_duplicate, parse_field, parse_str, parse_u64, parse_u8};
+use utils::{parse_date, parse_duplicate, parse_field, parse_sign, parse_str, Sign, parse_u64,
+            parse_u8};
 
 #[derive(PartialEq, Debug)]
 pub enum AccountStructure {
@@ -14,12 +15,6 @@ pub enum AccountStructure {
     ForeignAccountNumber,
     IBANBelgianAccountNumber,
     IBANForeignAccountNumber,
-}
-
-#[derive(PartialEq, Debug)]
-pub enum Sign {
-    Credit,
-    Debit,
 }
 
 #[allow(dead_code)]
@@ -46,14 +41,6 @@ impl OldBalance {
         }
     }
 
-    fn parse_sign(s: &str) -> Result<Sign> {
-        match s {
-            "0" => Ok(Sign::Credit),
-            "1" => Ok(Sign::Debit),
-            _ => Err(format!("Invalid Sign value [{}]", s).into()),
-        }
-    }
-
     pub fn parse(line: &str) -> Result<OldBalance> {
         Ok(OldBalance {
             account_structure: parse_field(line, 1..2, OldBalance::parse_accountstructure)
@@ -62,7 +49,7 @@ impl OldBalance {
                 .chain_err(|| "Could not parse old_sequence")?,
             account_currency: parse_field(line, 5..42, parse_str)
                 .chain_err(|| "Could not parse account_currency")?,
-            old_balance_sign: parse_field(line, 42..43, OldBalance::parse_sign)
+            old_balance_sign: parse_field(line, 42..43, parse_sign)
                 .chain_err(|| "Could not parse old_balance_sign")?,
             old_balance: parse_field(line, 43..58, parse_u64)
                 .chain_err(|| "Could not parse old_balance")?,
@@ -116,10 +103,79 @@ impl Header {
     }
 }
 
+/*
+MOVE_COMMON = {
+    'sequence': (slice(2, 6), str),
+    'detail_sequence': (slice(6, 10), str),
+    }
+MOVE = {
+    '1': {
+        'bank_reference': (slice(10, 31), str),
+        'amount': (slice(31, 47), _amount),
+        'value_date': (slice(47, 53), _date),
+        'transaction_code': (slice(53, 61), str),
+        '_communication': (slice(61, 115), str),
+        'entry_date': (slice(115, 121), _date),
+        'statement_number': (slice(121, 124), str),
+        },
+    '2': {
+        '_communication': (slice(10, 63), str),
+        'customer_reference': (slice(63, 98), _string),
+        'counterparty_bic': (slice(98, 109), _string),
+        'r_transaction': (slice(112, 113), _string),
+        'r_reason': (slice(113, 117), _string),
+        'category_purpose': (slice(117, 121), _string),
+        'purpose': (slice(121, 125), _string),
+        },
+    '3': {
+        'counterparty_account': (slice(10, 47), _string),
+        'counterparty_name': (slice(47, 82), _string),
+        '_communication': (slice(82, 125), str),
+        },
+    }
+*/
+
+#[allow(dead_code)]
+pub struct Movement1 {
+    pub sequence: String,         //': (slice(2, 6), str),
+    pub detail_sequence: String,  //': (slice(6, 10), str),
+    pub bank_reference: String,   //': (slice(10, 31), str),
+    pub amount: u64,              //': (slice(31, 47), _amount),
+    pub value_date: NaiveDate,    //': (slice(47, 53), _date),
+    pub transaction_code: String, //': (slice(53, 61), str),
+    pub communication: String,    //': (slice(61, 115), str),
+    pub entry_date: NaiveDate,    //': (slice(115, 121), _date),
+    pub statement_number: String, //': (slice(121, 124), str),
+}
+
+impl Movement1 {
+    fn parse(line: &str) -> Result<Movement1> {
+        Ok(Movement1 {
+            sequence: parse_field(line, 2..6, parse_str).chain_err(|| "Could not parse sequence")?,
+            detail_sequence: parse_field(line, 6..10, parse_str)
+                .chain_err(|| "Could not parse detail_sequence")?,
+            bank_reference: parse_field(line, 10..31, parse_str)
+                .chain_err(|| "Could not parse bank_reference")?,
+            amount: parse_field(line, 31..47, parse_u64).chain_err(|| "Could not parse amount")?,
+            value_date: parse_field(line, 47..53, parse_date)
+                .chain_err(|| "Could not parse value_date")?,
+            transaction_code: parse_field(line, 53..61, parse_str)
+                .chain_err(|| "Could not parse transaction_code")?,
+            communication: parse_field(line, 61..115, parse_str)
+                .chain_err(|| "Could not parse transaction_code")?,
+            entry_date: parse_field(line, 115..121, parse_date)
+                .chain_err(|| "Could not parse entry_date")?,
+            statement_number: parse_field(line, 121..124, parse_str)
+                .chain_err(|| "Could not parse statement_number")?,
+        })
+    }
+}
+
 #[allow(dead_code)]
 pub struct Coda {
     pub header: Header,
     pub old_balance: OldBalance,
+    pub movement1: Movement1,
 }
 
 impl Coda {
@@ -132,11 +188,13 @@ impl Coda {
         let reader = BufReader::new(f);
         let mut header: Option<Header> = None;
         let mut old_balance: Option<OldBalance> = None;
+        let mut movement1: Option<Movement1> = None;
         for line in reader.lines() {
             let l = line.unwrap();
             let t: u8 = match l.get(0..1) {
                 Some("0") => 0,
                 Some("1") => 1,
+                Some("2") => 2,
                 _ => 255,
             };
             match t {
@@ -147,17 +205,23 @@ impl Coda {
             },
             1 => {
                 old_balance = Some(OldBalance::parse(&l).chain_err(||->Error  {"Could not parse oldbalance".into()})?)
-            }
+            },
+            2 => {
+                movement1 = Some(Movement1::parse(&l).chain_err(||->Error  {"Could not parse Movement1".into()})?)
+            },
             _ => {}
             // _ => return Err("Unknown type".into()),
         }
         }
         if let Some(header) = header {
             if let Some(old_balance) = old_balance {
-                return Ok(Coda {
-                    header: header,
-                    old_balance: old_balance,
-                });
+                if let Some(movement1) = movement1 {
+                    return Ok(Coda {
+                        header: header,
+                        old_balance: old_balance,
+                        movement1: movement1,
+                    });
+                }
             }
         }
         Err("Could not parse code".into())
@@ -224,9 +288,9 @@ mod test_parse_header {
 mod test_parse_oldbalance {
     use chrono::NaiveDate;
 
+    use utils::Sign;
     use super::OldBalance;
     use super::AccountStructure;
-    use super::Sign;
 
     #[test]
     fn parse_oldbalance_valid() {
@@ -324,11 +388,54 @@ mod test_parse_oldbalance {
         let actual = OldBalance::parse_accountstructure("4");
         assert_eq!(actual.is_ok(), false, "'4' should not be ok");
     }
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod test_parse_movement1 {
+    use chrono::NaiveDate;
+
+    use super::Movement1;
 
     #[test]
-    fn parse_sign_valid_Credit() {
-        let actual = OldBalance::parse_sign("0");
-        assert_eq!(actual.is_ok(), true, "'0' should be ok");
-        assert_eq!(actual.unwrap(), Sign::Credit, "'0' should be Credit");
+    fn parse_movement1_valid() {
+        let line = "2100010000EPIB00048 AWIUBTKAPUO1000000002578250061206007990000BORDEREAU DE DECOMPTE AVANCES    015 NUMERO D'OPERATI06120600111 0";
+
+        let actual = Movement1::parse(line);
+
+        assert_eq!(actual.is_ok(), true, "Movement1 shoud be ok");
+        let actual = actual.unwrap();
+        assert_eq!(actual.sequence, "0001", "sequence should be '0001'");
+        assert_eq!(
+            actual.detail_sequence,
+            "0000",
+            "detail_sequence should be '0000'"
+        );
+        assert_eq!(
+            actual.bank_reference,
+            "EPIB00048 AWIUBTKAPUO",
+            "bank_reference should be 'EPIB00048 AWIUBTKAPUO'"
+        );
+        assert_eq!(
+            actual.amount,
+            1000000002578250,
+            "amount should be '1000000002578250'"
+        );
+        assert_eq!(
+            actual.value_date,
+            NaiveDate::from_ymd(2006, 12, 6),
+            "value_date should be '06/12/2006'"
+        );
+        assert_eq!(
+            actual.transaction_code,
+            "00799000",
+            "bank_reference should be '00799000'"
+        );
+        assert_eq!(
+            actual.communication,
+            "0BORDEREAU DE DECOMPTE AVANCES    015 NUMERO D\'OPERATI"
+        );
+        assert_eq!(actual.entry_date, NaiveDate::from_ymd(2006, 12, 6));
+        assert_eq!(actual.statement_number, "001");
     }
 }
